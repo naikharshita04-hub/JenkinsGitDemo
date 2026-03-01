9f25f903e6ac82d49a34fd87ffbc572260b4d00f
naikharshita04-hub <naikharshita04@gmail.com>
2026-03-02 00:04:29 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
+        System.out.println("Hello from GitHub - Poll SCM Working");
     }
 }
+
+
